1958e902bc54c21ae3020a04ee6afb5b9bd808e0
Ashutosh Agrahari <akagr.cse@gmail.comgit config --global user.email akagr.cse@gmail.com>
2018-08-04 23:22:41 +0530

    Updated with blogs, contacts and header.

--- build_site.R
+++ build_site.R
@@ -1,3 +1,5 @@
+# Source: http://nickstrayer.me/RMarkdown_Sites_tutorial/
+#         http://www.emilyzabor.com/tutorials/rmarkdown_websites_tutorial.html
 #Set our working directory. 
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
@@ -7,3 +9,4 @@ setwd("C:/Users/lenovo/Documents/R Projects/personol_site")
 
 #render your sweet site. 
 rmarkdown::render_site()
+
