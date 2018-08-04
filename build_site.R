c5218b0a0cee186cc0be84f3930ab8f9ca028148
Ashutosh Agrahari <akagr.cse@gmail.comgit config --global user.email akagr.cse@gmail.com>
2018-08-04 22:06:11 +0530

    Added few more tabs: 1. Education 2. Experience 3. Skill Set 4. Contact me

--- build_site.R
+++ build_site.R
@@ -2,6 +2,8 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
+setwd("C:/Users/lenovo/Documents/R Projects/personol_site")
+
 
 #render your sweet site. 
 rmarkdown::render_site()
